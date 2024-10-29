130afb3f41e8dd01dd18276f0f7bfe68496626c2
alee84sf <alee84@sfsu.edu>
2024-10-29 14:41:54 -0700

    sprinkler can sprinkle

--- SnowmanLauncher/Assets/Scripts/SprinklerScript.cs
+++ SnowmanLauncher/Assets/Scripts/SprinklerScript.cs
@@ -4,20 +4,58 @@ using UnityEngine;
 
 public class SprinklerScript : MonoBehaviour
 {
+    private bool visible;
+    private Transform child;
+    [SerializeField] GameObject sprinklerBullet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        visible = false;
+        child = this.gameObject.transform.GetChild(0);
+        StartCoroutine(RotateAndFire());
     }
 
-    // Update is called once per frame
-    void Update()
+    // sup
+    void FixedUpdate()
     {
         
     }
 
-    private void OnWillRenderObject()
+    public IEnumerator RotateAndFire()
     {
+        while(true)
+        {
+            //rotate, fire, wait
+            if(visible)
+            {
+                child.Rotate(0, 0, 30f, Space.Self);
+                //Vector3 v = child.position;
+
+                Instantiate(sprinklerBullet,child);
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    /*
+     *  If the sprinklers bug out when exporting to OpenGL,
+     *  determine visibility by checking distance from player
+     *  in FixedUpdate
+     *  instead of OnBecameVisible/Invisible
+     */
+
+    //flips visible/invisible based on camera render
+    //BE WARNED, it starts as invisible, so
+    //do NOT start sprinklers in LOS of the player
+    private void OnBecameVisible()
+    {
+        visible = true;
         Debug.Log(gameObject.name + " is visible!");
+    }
+    private void OnBecameInvisible()
+    {
+        visible = false;
+        Debug.Log(gameObject.name + " is invisible!");
     }
 }
